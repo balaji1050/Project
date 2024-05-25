d4e3e679f3419b3f7f2c6272434a5ce387a16deb
balaji1050 <balaji.d@ldappsolutions.com>
2024-05-25 13:38:31 +0530

    Fix:changed sum

--- Maximumthreenumbers.java
+++ Maximumthreenumbers.java
@@ -2,6 +2,7 @@ public class MaxOfThreeNumbers {
 
  public static void main(String[] args) {
 
+  int a=49, b= 20, sum:
  int num1 = 10, num2 = 20, num3 = 15, max;
 
  max = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
